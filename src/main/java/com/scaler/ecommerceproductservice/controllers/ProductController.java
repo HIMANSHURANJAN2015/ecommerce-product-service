ac2a2686dbc9f0de55dcbc5ebd21329498bc9ecb
HimanshuRanjan <livelikehimanshu12@gmail.com>
2025-02-24 20:09:32 +0530

    Added response entity in controller with proper http message

--- src/main/java/com/scaler/ecommerceproductservice/controllers/ProductController.java
+++ src/main/java/com/scaler/ecommerceproductservice/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.scaler.ecommerceproductservice.controllers;
 import com.scaler.ecommerceproductservice.dtos.ProductResponseDto;
 import com.scaler.ecommerceproductservice.models.Product;
 import com.scaler.ecommerceproductservice.services.FakeStoreProductService;
+import com.scaler.ecommerceproductservice.services.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     //I need to call FakeStoreProductService again and again, so store it as global variable
-    FakeStoreProductService fakeStoreProductService;
+    //To prevent tigh couploing b/w FakeStoreProductService and controller, I am using
+    //productService type. thus fulfilling Liskov Sibstitution PRinciple
+    ProductService productService;
 
-    public ProductController(FakeStoreProductService fakeStoreProductService) {
-        this.fakeStoreProductService = fakeStoreProductService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     //We have to give response back to client as ResponseEntity
-    @GetMapping("/product/{id}")
+    @GetMapping("/products/{id}")
     public ResponseEntity<ProductResponseDto> getProductById(@PathVariable long id) {
-        Product product = fakeStoreProductService.getProductById(id);
+        Product product = this.productService.getProductById(id);
         //We need to return back DTO, not entire model
         ProductResponseDto productResponseDto = ProductResponseDto.fromProduct(product);
-        return new ResponseEntity<>(productResponseDto,  HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(productResponseDto,  HttpStatus.OK);
     }
-
 }
